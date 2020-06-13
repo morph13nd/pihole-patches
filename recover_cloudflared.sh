a23e6aafc984a7b823a5391d6ec3952f658fa864
themaverick <35180652+themaverick@users.noreply.github.com>
2020-06-13 12:36:31 -0700

    Update recover_cloudflared.sh

--- recover_cloudflared.sh
+++ recover_cloudflared.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 #"cron is: */5 * * * *"
 #Written by https://github.com/themaverick/pihole-patches
-if pihole status | grep -q 'DNS is NOT running'; then
-  pihole restartdns
-  systemctl restart cloudflared.service
-  echo "DNS is NOT running at $(date)" >> /var/log/pihole/dns_uptime.log
-  else
-  echo "DNS is online at $(date)" >> /var/log/pihole/dns_uptime.log
-fi
+if pihole status | grep NOT; then echo "DNS is NOT running at $(date)" >> /var/log/pihole/dns_uptime.log && reboot; else echo "DNS is online at $(date)" >> /var/log/pihole/dns_uptime.log; fi
